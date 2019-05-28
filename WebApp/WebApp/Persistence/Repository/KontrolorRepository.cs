bfc748eea411f403049f220e36f0303cbedb84ca
Andrej Ivi?iak <E3111-2015@ftn.ns>
2019-05-28 12:30:52 +0200

    Repository changes
    Added Repo for Kontrolor

---
+++ WebApp/WebApp/Persistence/Repository/KontrolorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models.Users;
+
+namespace WebApp.Persistence.Repository
+{
+    public class KontrolorRepository : Repository<Kontrolor, int>, IKontrolorRepository
+    {
+        public KontrolorRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
