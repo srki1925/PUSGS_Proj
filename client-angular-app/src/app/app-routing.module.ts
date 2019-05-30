aa6d100a2040bd2848c6e4ac6a5f486ab972474c
Andrej Ivi?iak <E3111-2015@ftn.ns>
2019-05-30 11:24:53 +0200

    Client
    Added routing PogChamp
    Bootstrap for Logging is finaly working FeelsGoodMan

--- client-angular-app/src/app/app-routing.module.ts
+++ client-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {CreateUserComponent} from './Components/create-user/create-user.component'
+import{LoggingComponent} from './Components/logging/logging.component'
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/Login',
+    pathMatch:'full'
+},
+{
+  path:'CreateUser',
+  component:CreateUserComponent
+},
+{
+  path:'Login',
+  component:LoggingComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
