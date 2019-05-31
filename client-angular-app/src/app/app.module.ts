de60535a72bd0a18ecbc107b6452516c1a106885
Andrej Ivi?iak <E3111-2015@ftn.ns>
2019-05-31 09:32:32 +0200

    Client
    added folders for view and components

--- client-angular-app/src/app/app.module.ts
+++ client-angular-app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoggingComponent } from './Components/logging/logging.component';
 import { CreateUserComponent } from './Components/create-user/create-user.component';
+import { NavBarComponent } from './Components/view/nav-bar/nav-bar.component';
+import { PreviewComponent } from './Components/view/preview/preview.component';
+import { PriceListComponent } from './Components/view/previews/price-list/price-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoggingComponent,
-    CreateUserComponent
+    CreateUserComponent,
+    NavBarComponent,
+    PreviewComponent,
+    PriceListComponent
   ],
   imports: [
     BrowserModule,
