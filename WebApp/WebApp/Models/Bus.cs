ca5a2e520e258fc3b36d918b998e7e3a9f1eace0
srki1925 <srki1925@gmail.com>
2019-06-01 15:09:02 +0200

    Model modify #2

--- WebApp/WebApp/Models/Bus.cs
+++ WebApp/WebApp/Models/Bus.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace WebApp.Models
 {
-    public class Autobus
-    {
+	public class Bus
+	{
+		public Bus()
+		{
+		}
 
-        public Autobus()
-        {
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
 
-        }
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        public int Naziv { get; set; }
-        public string Longitude { get; set; }
-        public string Latitude { get; set; }
-        [ForeignKey("Linija")]
-        public int? Linija_Id { get; set; }
-        public Line Linija { get; set; }
-        public bool Active { get; set; }
-    }
+		public int Naziv { get; set; }
+		public string Longitude { get; set; }
+		public string Latitude { get; set; }
+
+		[ForeignKey("Linija")]
+		public int? Linija_Id { get; set; }
+
+		public Line Linija { get; set; }
+		public bool Active { get; set; }
+	}
 }
