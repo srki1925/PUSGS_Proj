75eeb2d0382d1a00b26a25981622efbdf4f9a28f
TheLastHop3 <39307359+TheLastHop3@users.noreply.github.com>
2019-06-12 12:28:22 +0200

    Update passenger.guard.ts

--- WebApp/WebApp/FrontEndApp/FrontEndApp/src/app/route-guards/passenger.guard.ts
+++ WebApp/WebApp/FrontEndApp/FrontEndApp/src/app/route-guards/passenger.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { basename } from 'path';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
